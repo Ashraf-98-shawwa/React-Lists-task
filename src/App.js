70e4866b1c8b079bcc5f8d46077339183879caa9
Ashraf shawwa <ashraf_shawa@hotmail.com>
2022-12-21 15:50:01 +0200

    React lists task

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import Counter from "./components/counter";
+import "./App.css";
+
+import React, { Component } from "react";
+
+const counters = [
+  { id: 1, count: 0, steps: 1 },
+  { id: 2, count: 0, steps: 2 },
+  { id: 3, count: 0, steps: 3 },
+  { id: 4, count: 0, steps: 4 },
+];
+
+export default class App extends Component {
+  state = {
+    counters,
+  };
+
+  onIncrement = (id, steps = 1) => {
+    this.setState((prevState) => {
+      return {
+        counters: prevState.counters.map((item) => {
+          if (item.id === id) {
+            return { ...item, count: item.count + steps };
+          }
+          return item;
+        }),
+      };
+    });
+  };
+
+  onDecrement = (id, steps = 1) => {
+    this.setState((prevState) => {
+      return {
+        counters: prevState.counters.map((item) => {
+          if (item.id === id) {
+            if (item.count > 0) {
+              return { ...item, count: item.count - steps };
+            }
+            return item;
+          }
+          return item;
+        }),
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.counters.map((item) => (
+          <Counter
+            key={item.id}
+            {...item}
+            onDecrement={this.onDecrement}
+            onIncrement={this.onIncrement}
+          />
+        ))}
+      </div>
+    );
+  }
+}
